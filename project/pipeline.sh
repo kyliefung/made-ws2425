9e67772103cd5a9203faa64c44e4c2464e6db6a7
kyliefung <kyliefung2022@gmail.com>
2024-11-14 17:37:03 +0100

    Add Data-Pipeline.py for ETL process

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,2 +1,11 @@
+# Ensure that you have all the required Python packages installed. 
+# Please ensure that you have downloaded the below Python package. 
+# requests
+# pandas
+# kaggle
+# sqlalchemy
+# selenium
+# beautifulsoup4
+
 #!/bin/bash
 python3 project/Data-Pipeline.py
